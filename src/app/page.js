f28baa049117aced0359a3f2a8b207d88cc6cc31
Jacob Erbele <jacob.erbele@gmail.com>
2024-11-04 23:00:36 -0600

    Changed to render

--- src/app/page.js
+++ src/app/page.js
@@ -78,7 +78,7 @@ const EventRow = ({ event, rosterUrl }) => {
 
 async function fetchEvents() {
   try {
-    const response = await fetch('http://hockey-calendar.vercel.app/api/events');
+    const response = await fetch('https://hockey-calendar.onrender.com/api/events');
     if (!response.ok) {
       throw new Error('Failed to fetch events');
     }
