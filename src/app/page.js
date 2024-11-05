031cde512ad4effc3acca93a120a68f42755a65d
Jacob Erbele <jacob.erbele@gmail.com>
2024-11-04 22:57:37 -0600

    Change API call to vercel

--- src/app/page.js
+++ src/app/page.js
@@ -78,7 +78,7 @@ const EventRow = ({ event, rosterUrl }) => {
 
 async function fetchEvents() {
   try {
-    const response = await fetch('http://localhost:3001/api/events');
+    const response = await fetch('http://hockey-calendar.vercel.app/api/events');
     if (!response.ok) {
       throw new Error('Failed to fetch events');
     }
