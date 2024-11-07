068181d419f27fca9f270c4aadac95fb83cdbeea
Jacob Erbele <jacob.erbele@gmail.com>
2024-11-06 19:17:42 -0600

    Updated backend server address

--- src/app/page.js
+++ src/app/page.js
@@ -56,7 +56,7 @@ const EventRow = ({ event, rosterUrl }) => {
 
 async function fetchEvents() {
   try {
-    const response = await fetch('http://localhost:3001/api/events');
+    const response = await fetch('https://hockey-calendar.onrender.com/api/events');
     if (!response.ok) {
       throw new Error('Failed to fetch events');
     }
